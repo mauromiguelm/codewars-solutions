ddf8c0de91b7d582daa3aaa1e50db6a5f5734605
Mauro Masiero <mauromiguelm@hotmail.com>
2022-05-07 15:09:38 +0200

    fixed escape chars

--- String ends with/main.py
+++ String ends with/main.py
@@ -1,3 +1,3 @@
 import re
 def solution(string, ending):
-    return bool(re.search( r"({}$)".format(ending), string))
+    return bool(re.search( r"({}$)".format(re.escape(ending)), re.escape(string)))
